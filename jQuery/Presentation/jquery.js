dc31dba43d3d67de29587a72ce808e4aa194002d
Sarah Boye Grundahl <koibito2602@gmail.com>
2018-05-28 10:56:43 +0200

    changed stuff in wordpress & added example code to jQuery project

---
+++ jQuery/Presentation/jquery.js
@@ -0,0 +1,36 @@
+$( function() {
+    $( "#accordion" ).accordion({
+    heightStyle: "content"
+    });
+} );
+
+$( function() {
+    var state = true;
+    $( "#button" ).on( "click", function() {
+    if ( state ) {
+        $( "#effect" ).animate({
+        backgroundColor: "#aa0000",
+        color: "#fff",
+        width: 500
+        }, 1000 );
+    } else {
+        $( "#effect" ).animate({
+        backgroundColor: "#fff",
+        color: "#000",
+        width: 240
+        }, 1000 );
+    }
+    state = !state;
+    });
+} );
+
+$( function() {
+    $( "#sortable" ).sortable({
+    placeholder: "ui-state-highlight"
+    });
+    $( "#sortable" ).disableSelection();
+} );
+
+$( function() {
+    $( "input" ).checkboxradio();
+  } );
